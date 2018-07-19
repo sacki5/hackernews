bc5f7b52296addc3d5c08f5c1c4585d127871802
Isac Ljung <isac.ljung@gmail.com>
2018-07-19 10:46:48 +0200

    added express server for publish

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/hackernews/'));
 
 
 
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
 
-app.get('/*', () => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/hackernews/index.html'));
 });
 
 console.log('Sever up on port');
