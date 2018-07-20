963070c61bf275bbe56330e45a8fb5963786c834
Isac Ljung <isac.ljung@gmail.com>
2018-07-20 11:35:17 +0200

    Fixed port issue for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/dist/hackernews/'));
 
 
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/hackernews/index.html'));
